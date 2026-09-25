5a5e034a801009f8ae0cd75da98682aa21a0856c
johnpetedoe <johnpetedoe@proton.me>
2026-09-24 18:09:44 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,49 @@
+#include<stdbool.h>
+
+typedef t_N typeof_N; 
+
+typedef struct s_N      {} t_N;
+typedef struct s_wall   {} t_wall;
+typedef struct s_door   {} t_door;
+typedef struct s_window {} t_window;
+typedef str
+typedef struct s_system 
+
+//11111 11111
+//11111 11111
+//11111 11111
+
+//11111 11111
+//11111 11111
+//11111 11111
+  
+// =d0  //d{0..2}
+// =z 1
+  
+//111111
+//111111
+//111111
+
+//111111
+//111111
+//111111
+
+//111111
+//111111
+//111111
+
+//111111
+//111111
+//111111
+
+
+bool  f(char*** static_laberynth, int start[3], int* seq) 
+{
+
+}
+
+// this array of matrices is generated via formal derivation. 
+bool g(char**** pseudo_dynamic_laberynth, int start[4], int* seq)
+{
+  
+}
